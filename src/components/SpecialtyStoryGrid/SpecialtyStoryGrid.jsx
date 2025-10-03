933f187b22230e69d558db441700e356a998190e
Stéfanie LOISELEUR <stloiseleur@ippon.fr>
2025-10-03 17:53:49 +0200

    Exercise 4: Specialty Story grid

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.jsx
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.jsx
@@ -6,6 +6,7 @@ import { MARKET_DATA, SPORTS_STORIES } from '../../data';
 import MarketCard from '../MarketCard';
 import SectionTitle from '../SectionTitle';
 import MiniStory from '../MiniStory';
+import {QUERIES} from "../../constants.js";
 
 const SpecialtyStoryGrid = () => {
   return (
@@ -34,11 +35,13 @@ const SpecialtyStoryGrid = () => {
         >
           Sports
         </SectionTitle>
-        <SportsStories>
-          {SPORTS_STORIES.map((data) => (
-            <MiniStory key={data.id} {...data} />
-          ))}
-        </SportsStories>
+        <OverflowWrapper>
+          <SportsStories>
+            {SPORTS_STORIES.map((data) => (
+              <MiniStory key={data.id} {...data} />
+            ))}
+          </SportsStories>
+        </OverflowWrapper>
       </SportsSection>
     </Wrapper>
   );
@@ -47,14 +50,50 @@ const SpecialtyStoryGrid = () => {
 const Wrapper = styled.div`
   display: grid;
   gap: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: 1fr 1fr;
+    gap: 0px;
+  }
 `;
 
-const MarketsSection = styled.section``;
+const MarketsSection = styled.section`
 
-const MarketCards = styled.div``;
+
+  @media ${QUERIES.laptopAndUp} {
+    border-right: 1px solid var(--color-gray-300);
+    margin-right: 16px;
+    padding-right: 16px;
+  }
+`;
+
+const MarketCards = styled.div`
+  display: grid;
+  gap: 16px;
+  grid-template-columns:
+      repeat(auto-fill, minmax(170px, 1fr));
+`;
 
 const SportsSection = styled.section``;
 
-const SportsStories = styled.div``;
+const SportsStories = styled.div`
+  display: grid;
+  gap: 16px;
+  grid-template-columns: repeat(auto-fill, minmax(170px, 1fr));
+
+  @media ${QUERIES.tabletAndUp} {
+    display: flex;
+    gap: 16px;
+    max-width: 100%;
+    overflow: auto;
+  }
+`;
+
+const OverflowWrapper = styled.div`
+  @media ${QUERIES.tabletAndUp} {
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+`;
 
 export default SpecialtyStoryGrid;
